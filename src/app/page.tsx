1912d362140b46cf2d7647517208e4a485d74745
pengxinseo <208015671@qq.com>
2024-06-07 09:45:33 +0800

    增加UI组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="border border-red-400">
-      我是测试
+
+      <Button>Click me</Button>
+      demo zhengpengxin  汉子测试
     </div>
   );
 }
