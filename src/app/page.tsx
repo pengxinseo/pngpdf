e9af8a626ba321126872c1c13c90b2bf7b836eb3
pengxinseo <208015671@qq.com>
2024-06-13 15:57:18 +0800

    1223

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -342,19 +342,6 @@ const Home = () => {
               )
             })
           }
-          <div className="flex flex-col border justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-                  <div className="p-5">
-                    <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-gray-100">
-                      <Icon4/>
-                    </div>
-                    <p className="mb-2 font-bold">我是测试的</p>
-                    <p className="text-sm leading-5 text-gray-900">
-                      我是测试的内容
-                    </p>
-                  </div>
-                  <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
-                </div>
-
         </div>
       </div>
 
