ca353e6116f827c22564445709a402033c74e28d
pengxinseo <208015671@qq.com>
2024-06-15 16:19:39 +0800

    新增多语言的版本

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import PageComponent from "./PageComponent";
 import { getIndexLanguageText } from "@/configs/languageText"
 import {unstable_setRequestLocale} from 'next-intl/server';
 
+export const runtime = 'edge';
 export default async function IndexPage({params: {locale = ''}}) {
   unstable_setRequestLocale(locale);
   const indexLanguageText = await getIndexLanguageText();
